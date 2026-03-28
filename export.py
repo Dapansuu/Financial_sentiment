e3d28977666f534fd7ce1813e465928750701d6f
dapansuu <dapansuu@gmail.com>
2026-03-28 19:00:00 +0000

    improved

--- export.py
+++ export.py
@@ -1,6 +1,12 @@
 import tensorflow as tf
+import os
 
 model = tf.keras.models.load_model('models/sentiment.keras')
+version = os.listdir("serving_model/sentiment")
+print(version)
 
-export_dir = "serving_model/sentiment/1"
+max_version = max([int(i) for i in version]) if version else 0
+version = max_version + 1
+print(f"Exporting model version: {version}")
+export_dir = f"serving_model/sentiment/{version}"
 model.export(export_dir)
